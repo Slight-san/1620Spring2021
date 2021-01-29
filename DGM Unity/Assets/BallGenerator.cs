3cbf107741d48eead1e1eda364c35d1ba16eb5c8
Bryson Tedesco <10802552@my.uvu.edu>
2021-01-29 10:57:12 -0700

    DGM Unity

---
+++ DGM Unity/Assets/BallGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallGenerator : MonoBehaviour
+{
+    public GameObject Ball;
+    
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.5f);
+            Instantiate(Ball);
+        }
+    }
+}
