18b5cda49e9ee391d74655f627d9d441750df82a
Slight-san <10802552@my.uvu.edu>
2021-02-08 10:34:50 -0700

    Counter Script

---
+++ DGM Unity/Assets/Counter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+   
+    public Int numberCount;
+    public Text counterText;
+        
+   
+    void Update()
+    {
+		numberCount++;
+		counterText.text = numberCount.ToString();
+        
+    }
+}
