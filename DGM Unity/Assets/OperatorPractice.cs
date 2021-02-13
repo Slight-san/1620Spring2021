18bca17908ab9af45052edf626613fac61021efd
Slight-san <10802552@my.uvu.edu>
2021-02-12 23:10:54 -0700

    Operator practice

---
+++ DGM Unity/Assets/OperatorPractice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int x = 100 + 50;
+    public static int sum1 = 80 + 80; 
+    
+    public static int sum2 = sum1 + 150;
+    
+    public static int sum3 = sum2 * sum1;
+
+    // errors in unity for invalid use of "{"?
+    {
+        int y = sum3;
+        Console.WriteLine(y);
+    }
+    
+}
+// error: "{", "(", Tuple?, ";"
