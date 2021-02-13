8cc0e80907d0bb491cf4e6b2dabce9a854177e86
Slight-san <10802552@my.uvu.edu>
2021-02-12 23:11:33 -0700

    Variable practice V2

---
+++ DGM Unity/Assets/Variable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class Variable : MonoBehaviour
+
+{
+        {
+             string name = "John";
+             Console.WriteLine(name);
+        }
+        
+        {
+            int nyNum = 15;
+            Console.WriteLine(nyNum);
+        }
+        
+        {
+            int myNumm = 27;
+            int myNumb = 10;
+            Console.WriteLine((myNumm)-(myNumb));
+        }
+}
