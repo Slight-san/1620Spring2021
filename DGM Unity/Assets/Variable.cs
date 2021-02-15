a5de8155061f485464495057a8931646ba74344e
Slight-san <10802552@my.uvu.edu>
2021-02-14 20:45:58 -0700

    More practice, deactivated 2 lines (for now)

--- DGM Unity/Assets/Variable.cs
+++ DGM Unity/Assets/Variable.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 public class Variable : MonoBehaviour
 
 {
-        {
-             string name = "John";
-             Console.WriteLine(name);
-        }
+    {
+        string name = "John";
+        //Console.WriteLine(name);
+    }
+
+    {
+        int nyNum = 15;
+        //Console.WriteLine(nyNum);
+    }
+
+    {
+        int myNumm = 27;
+        int myNumb = 10;
+        int myNun = ((myNumm) - (myNumb));
+        Console.WriteLine((myNun));
         
-        {
-            int nyNum = 15;
-            Console.WriteLine(nyNum);
-        }
-        
-        {
-            int myNumm = 27;
-            int myNumb = 10;
-            Console.WriteLine((myNumm)-(myNumb));
-        }
-}
+    }
+}       
