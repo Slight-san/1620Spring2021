e3dd5b503433d3d0853c349ca908c52fb83af6a6
Slight-san <10802552@my.uvu.edu>
2021-02-17 10:49:15 -0700

    Boost Behaviour and gatekeeperWIP

---
+++ DGM Unity Project/Assets/Scripts/BoostBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class BoostBehaviour : MonoBehaviour
+{
+    public bool hasKey;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (hasKey)
+        {
+            print(message:"Boost");
+        }
+    }
+}
---
+++ DGM Unity Project/Assets/Scripts/GateKeeperBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class GateKeeperBehaviour : MonoBehaviour
+{
+    public BoolData keyObj; //create "BoolData"
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (keyObj.value) //value script needed
+        {
+            print(true);
+        }
+        else
+        {
+            print(false);
+        }
+    }
+}
