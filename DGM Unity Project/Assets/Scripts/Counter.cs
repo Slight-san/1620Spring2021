8eec5ac959df3340dae3091842038b656d0fce82
Slight-san <10802552@my.uvu.edu>
2021-02-25 11:16:00 -0700

    counter UI

---
+++ DGM Unity Project/Assets/Scripts/Counter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    public int coinValue = 5;
+    public int numberCount;
+    public Text counterText;
+
+    void OnTriggerEnter2D()
+    {
+        numberCount += coinValue;
+        counterText.text = numberCount.ToString();
+        gameObject.SetActive(false);
+    }
+}
