e64bd9e198232b382ca53ab5f6cf552086b8d03f
Slight-san <10802552@my.uvu.edu>
2021-02-24 20:49:49 -0700

    Count Down Behaviour practice

---
+++ DGM Unity Project/Assets/Scripts/CountDown.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour
+{
+    public int number = 3;
+    public float holdTime = 1f;
+    public Text countDownText;
+    
+    private WaitForSeconds wfsObj;
+    
+    IEnumerator Start()
+    {
+        wfsObj = new WaitForSeconds(holdTime);
+        
+        while (number > 0)
+        {
+            countDownText.text = number.ToString();
+            number--;
+            yield return wfsObj;
+        }
+
+        countDownText.text = "Go!";
+
+        yield return wfsObj;
+
+        countDownText.text = null;
+    }
+}
