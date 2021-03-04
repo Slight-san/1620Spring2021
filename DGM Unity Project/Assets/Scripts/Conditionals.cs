3a2a7c89ed1e7953689d5d23d8549e8ce3b07cdb
Slight-san <10802552@my.uvu.edu>
2021-03-03 20:52:27 -0700

    Conditionals practice

---
+++ DGM Unity Project/Assets/Scripts/Conditionals.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditionals : MonoBehaviour
+{
+    private int time = 50;
+    private int space = 36;
+    
+   // if (time > 20)
+    {
+        Console.Writeline("time is forever");
+    }
+   // else
+    {
+        Console.Writeline("space is infinite");
+    }
+}
+
+
