60a30568880a2354b915f15fd7299ec4581eeec2
Slight-san <10802552@my.uvu.edu>
2021-03-03 20:59:42 -0700

    Switch practice

---
+++ DGM Unity Project/Assets/Scripts/SwitchPractice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class SwitchPractice : MonoBehaviour
+{
+    private int day = 5;
+
+   // Switch(day)
+    {
+        case 1:
+            Console.WriteLine("Monday");
+            BatchBreakingReason
+        case 2:
+            Console.WriteLine("Tuesday");
+            BatchBreakingReason;
+        case 3:
+            Console.WriteLine("wednesday");
+            BatchBreakingReason;
+        case 4:
+            Console.WriteLine("Thursday");
+            BatchBreakingReason;
+        case 5:
+            Console.WriteLine("Friday");
+            BatchBreakingReason;
+        case 6:
+            Console.WriteLine("Saturday");
+            BatchBreakingReason;
+        case 7:
+            Console.WriteLine("Sunday");
+            BatchBreakingReason;
+            //output is "Friday"
+
+    }
+}
