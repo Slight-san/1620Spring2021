71c2fe07a80a0e1021244acf82bacdf9d68166df
Slight-san <10802552@my.uvu.edu>
2021-03-05 10:57:03 -0700

    forloop with arrays

---
+++ DGM Unity Project/Assets/Scripts/ForLoopsPractice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForLoopsPractice : MonoBehaviour
+{
+    private string[] days = {"Monday", "Tuesday", "Wednesday","Thursday"};
+    for (int i = 0; i < days.Length; i++)
+    {
+        Console.WriteLine(days[i]);
+    }    
+}
