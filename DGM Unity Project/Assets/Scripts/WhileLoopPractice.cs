702bb582b7c56193f3a126d798c406eaaa268296
Slight-san <10802552@my.uvu.edu>
2021-03-05 10:41:38 -0700

    While Loop practice

---
+++ DGM Unity Project/Assets/Scripts/WhileLoopPractice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileLoopPractice : MonoBehaviour
+{
+    private int x = 0;
+    void Start()
+    {
+        while (x < 10)
+        {
+            Console.WriteLine(x);
+            x++;
+        }
+    }
+    
+}
