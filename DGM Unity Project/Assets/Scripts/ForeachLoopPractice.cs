b788668fcbf0e485d8150764c1c85ec5b06429b7
Slight-san <10802552@my.uvu.edu>
2021-03-05 11:02:30 -0700

    ForeachLoop practice

---
+++ DGM Unity Project/Assets/Scripts/ForeachLoopPractice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForeachLoopPractice : MonoBehaviour
+{
+    private string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+    ForeachLoopPractice(string i in cars)
+    {
+        Console.WriteLine(i);
+    }
+}
