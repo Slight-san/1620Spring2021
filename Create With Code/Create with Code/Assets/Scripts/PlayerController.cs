c405f3547d56f42f59b0f8e5456dc56d87adcde1
Slight-san <10802552@my.uvu.edu>
2021-03-05 18:54:09 -0700

    player controller

---
+++ Create With Code/Create with Code/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    void Update()
+    {
+        //Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+    }
+}
