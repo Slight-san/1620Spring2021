483d7ec28a8db53a141698994ff3f70f7d51d0e5
Slight-san <10802552@my.uvu.edu>
2021-03-24 22:55:10 -0600

    Updated playercontroller with "xRange"

--- Create With Code/Prototype 2/Assets/Scripts/PlayerController.cs
+++ Create With Code/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -6,12 +6,19 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
+    public float xRange = 12.0f;
     void Update()
     {
-        if (transform.position.x < -10)
+        if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
+        
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        
        horizontalInput = Input.GetAxis("Horizontal");
        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
