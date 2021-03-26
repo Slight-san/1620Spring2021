a36dd84aec9a17b755bd1593c533637a2b6e9e3c
Slight-san <10802552@my.uvu.edu>
2021-03-25 22:08:39 -0600

    destroy out of bounds script

---
+++ Create With Code/Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBound = 35;
+    private float botBound = -12;
+    void Update()
+    {
+        if(transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        } 
+        else if (transform.position.z < botBound)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
