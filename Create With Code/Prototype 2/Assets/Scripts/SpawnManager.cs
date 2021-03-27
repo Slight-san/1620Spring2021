3e082d48d1f9f61aef00186006ab7c63b5e0a4f6
Slight-san <10802552@my.uvu.edu>
2021-03-27 17:58:32 -0600

    auto-spawn and invokerepeating added

--- Create With Code/Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Create With Code/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -7,15 +7,19 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 34;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
 
-    void Update()
+    void Start()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+    
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
             
-            Instantiate(animalPrefabs[animalIndex], spawnPos ,animalPrefabs[animalIndex].transform.rotation);
-        }
+        Instantiate(animalPrefabs[animalIndex], spawnPos ,animalPrefabs[animalIndex].transform.rotation);
     }
 }
