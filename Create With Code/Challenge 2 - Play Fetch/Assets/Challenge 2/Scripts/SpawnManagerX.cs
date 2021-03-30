fa12489a4a3820127c1aaef1b682cd69f933e97c
Slight-san <10802552@my.uvu.edu>
2021-03-29 23:05:01 -0600

    Fixed script to spawn a random ball & spawn within a random time limit

---
+++ Create With Code/Challenge 2 - Play Fetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -20;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float minInterval = 1.0f;
+    private float maxInterval = 3.0f;
+    
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBall", startDelay, Random.Range( minInterval, maxInterval));
+    }
+
+   
+    void SpawnRandomBall ()
+    {
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+        
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+    }
+
+}
