4ae18df7c82c734414d3f65cb975e532bce90742
Slight-san <10802552@my.uvu.edu>
2021-03-29 23:05:47 -0600

    Fixed script to actually destroy objects out of bounds

---
+++ Create With Code/Challenge 2 - Play Fetch/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBoundsX : MonoBehaviour
+{
+    private float leftLimit = -29;
+    private float bottomLimit = -5;
+    
+    void Update()
+    {
+        
+        if (transform.position.x < leftLimit)
+        {
+            Destroy(gameObject);
+        } 
+        
+        else if (transform.position.y < bottomLimit)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
