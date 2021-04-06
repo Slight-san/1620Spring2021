33460c926daf4479970d7a3728afec07a080efab
Slight-san <10802552@my.uvu.edu>
2021-04-05 23:00:42 -0600

    Added repeatWidth float to make bg repeat smoothly

--- Create With Code/Prototype 3/Assets/Scripts/RepeatBackground.cs
+++ Create With Code/Prototype 3/Assets/Scripts/RepeatBackground.cs
@@ -3,16 +3,18 @@
 public class RepeatBackground : MonoBehaviour
 {
     private Vector3 startPos;
+    private float repeatWidth;
   
     void Start()
     {
         startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
     }
 
     
     void Update()
     {
-        if (transform.position.x < startPos.x - 55)
+        if (transform.position.x < startPos.x - repeatWidth)
         {
             transform.position = startPos;
         }
