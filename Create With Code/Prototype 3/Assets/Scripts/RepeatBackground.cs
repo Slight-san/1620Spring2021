f8bcd641ed0313e9e17f12d59634f33d23c90c8a
Slight-san <10802552@my.uvu.edu>
2021-04-05 22:56:09 -0600

    Added repeatBackground script

---
+++ Create With Code/Prototype 3/Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    private Vector3 startPos;
+  
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    
+    void Update()
+    {
+        if (transform.position.x < startPos.x - 55)
+        {
+            transform.position = startPos;
+        }
+    }
+}
