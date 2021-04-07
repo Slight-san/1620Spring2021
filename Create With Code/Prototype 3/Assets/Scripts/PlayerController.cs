ffbe2b9b6bae3a270f1985253c06330bc7915619
Slight-san <10802552@my.uvu.edu>
2021-04-06 21:47:47 -0600

    Added collision detection for "Game Over" message

--- Create With Code/Prototype 3/Assets/Scripts/PlayerController.cs
+++ Create With Code/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver;
  
     void Start()
     {
@@ -28,6 +29,17 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+
+        else if(collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
+        }
     }
 }
