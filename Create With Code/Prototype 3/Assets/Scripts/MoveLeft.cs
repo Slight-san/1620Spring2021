e36cc0c1f94957f95cf4349739f807e092a226b9
Slight-san <10802552@my.uvu.edu>
2021-04-06 21:54:35 -0600

    Added code to stop moveleft script when player collision happens

--- Create With Code/Prototype 3/Assets/Scripts/MoveLeft.cs
+++ Create With Code/Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float speed = 30;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private PlayerController playerControllerScript;
 
-    // Update is called once per frame
+    void start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+    
+    
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        
     }
 }
