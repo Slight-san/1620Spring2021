a7f3a20d141bc8fe43ad395a2789a8dd350746df
Slight-san <10802552@my.uvu.edu>
2021-04-10 10:29:02 -0600

    made it so player can't jump once the game is over

--- Create With Code/Prototype 3/Assets/Scripts/PlayerController.cs
+++ Create With Code/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -22,7 +22,7 @@ public class PlayerController : MonoBehaviour
     
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce , ForceMode.Impulse);
             isOnGround = false;
