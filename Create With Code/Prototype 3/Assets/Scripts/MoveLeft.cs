ab7ef9875e23ff8c4d0a339b6ebd0ce74cf08546
Slight-san <10802552@my.uvu.edu>
2021-04-10 09:56:28 -0600

    added "destroy object" for obstacles

--- Create With Code/Prototype 3/Assets/Scripts/MoveLeft.cs
+++ Create With Code/Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -6,8 +6,9 @@ public class MoveLeft : MonoBehaviour
 {
     private float speed = 30;
     private PlayerController playerControllerScript;
+    private float leftBound = -15;
 
-    void start()
+    void Start()
     {
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
@@ -18,6 +19,11 @@ public class MoveLeft : MonoBehaviour
         if (playerControllerScript.gameOver == false)
         {
             transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
         }
         
     }
