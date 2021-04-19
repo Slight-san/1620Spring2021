0e734d426c9c67ac7fb8d5caff64ddadf9510008
Slight-san <10802552@my.uvu.edu>
2021-04-18 21:04:18 -0600

    added enemy script to follow player around

---
+++ Create With Code/Prototype 4/Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody enemyRb;
+    private GameObject player;
+    public float speed = 3.0f;
+    
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    
+    void Update()
+    {
+        enemyRb.AddForce((player.transform.position - transform.position).normalized * speed);
+    }
+}
