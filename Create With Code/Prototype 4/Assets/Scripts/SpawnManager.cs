bdd6698a36300ba086eefa4caf869e2445d49358
Slight-san <10802552@my.uvu.edu>
2021-04-21 23:18:18 -0600

    created custom method to get random position, code cleanup

--- Create With Code/Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Create With Code/Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -7,16 +7,16 @@ public class SpawnManager : MonoBehaviour
    
     void Start()
     {
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+    }
+    
+    private Vector3 GenerateSpawnPosition()
+    {
         float spawnPosX = Random.Range(-spawnRange, spawnRange);
         float spawnPosZ = Random.Range(-spawnRange, spawnRange);
 
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
-        
-        Instantiate(enemyPrefab, randomPos, enemyPrefab.transform.rotation);
-    }
-    
-    void Update()
-    {
-        
+
+        return randomPos;
     }
 }
