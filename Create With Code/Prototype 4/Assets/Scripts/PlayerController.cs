70b88c36e1bf6675fe2b6ed980e4a77fce211bb0
Slight-san <10802552@my.uvu.edu>
2021-04-24 19:08:58 -0600

    destroy powerup on contact, and set bool to true

--- Create With Code/Prototype 4/Assets/Scripts/PlayerController.cs
+++ Create With Code/Prototype 4/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     private GameObject focalPoint;
     public float speed = 5.0f;
+    public bool hasPowerup = false;
     
 
     void Start()
@@ -18,5 +20,14 @@ public class PlayerController : MonoBehaviour
         float forwardInput = Input.GetAxis("Vertical");
         
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
     }
 }
