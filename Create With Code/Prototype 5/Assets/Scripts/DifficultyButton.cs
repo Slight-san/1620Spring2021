7b012192d319fe7a7328473915cb961d7a7aa69c
Slight-san <10802552@my.uvu.edu>
2021-04-29 19:57:36 -0600

    added game manager reference, called start game method so the game only starts after a difficulty button has been pressed

--- Create With Code/Prototype 5/Assets/Scripts/DifficultyButton.cs
+++ Create With Code/Prototype 5/Assets/Scripts/DifficultyButton.cs
@@ -5,10 +5,13 @@ using UnityEngine.UI;
 public class DifficultyButton : MonoBehaviour
 {
     private Button button;
+    private GameManager gameManger;
    
     void Start()
     {
         button = GetComponent<Button>();
+        gameManger = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        
         button.onClick.AddListener(SetDifficulty);
     }
 
@@ -21,5 +24,7 @@ public class DifficultyButton : MonoBehaviour
     void SetDifficulty()
     {
         Debug.Log(button.gameObject.name + " was ckicked");
+        gameManger.StartGame();
+        
     }
 }
