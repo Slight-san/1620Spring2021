197378016cb332746926f4ed519a69dfbff99d05
Slight-san <10802552@my.uvu.edu>
2021-04-29 20:46:18 -0600

    code cleanup

--- Create With Code/Challenge 5/Assets/Challenge 5/Scripts/DifficultyButtonX.cs
+++ Create With Code/Challenge 5/Assets/Challenge 5/Scripts/DifficultyButtonX.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class DifficultyButtonX : MonoBehaviour
@@ -9,7 +7,7 @@ public class DifficultyButtonX : MonoBehaviour
     private GameManagerX gameManagerX;
     public int difficulty;
 
-    // Start is called before the first frame update
+   
     void Start()
     {
         gameManagerX = GameObject.Find("Game Manager").GetComponent<GameManagerX>();
@@ -17,9 +15,7 @@ public class DifficultyButtonX : MonoBehaviour
         button.onClick.AddListener(SetDifficulty);
     }
 
-    /* When a button is clicked, call the StartGame() method
-     * and pass it the difficulty value (1, 2, 3) from the button 
-    */
+   
     void SetDifficulty()
     {
         Debug.Log(button.gameObject.name + " was clicked");
