a36d2d6f7ce93451d8be97da42e58eab7323c936
Slight-san <10802552@my.uvu.edu>
2021-04-29 20:47:25 -0600

    code cleanup

--- Create With Code/Challenge 5/Assets/Challenge 5/Scripts/GameManagerX.cs
+++ Create With Code/Challenge 5/Assets/Challenge 5/Scripts/GameManagerX.cs
@@ -19,10 +19,9 @@ public class GameManagerX : MonoBehaviour
     public bool isGameActive;
 
     private float spaceBetweenSquares = 2.5f; 
-    private float minValueX = -3.75f; //  x value of the center of the left-most square
-    private float minValueY = -3.75f; //  y value of the center of the bottom-most square
+    private float minValueX = -3.75f; 
+    private float minValueY = -3.75f; 
     
-    // Start the game, remove title screen, reset score, and adjust spawnRate based on difficulty button clicked
     public void StartGame(int difficulty)
     {
         spawnRate /= difficulty;
@@ -32,8 +31,7 @@ public class GameManagerX : MonoBehaviour
         UpdateScore(0);
         titleScreen.SetActive(false);
     }
-
-    // While game is active spawn a random target
+    
     IEnumerator SpawnTarget()
     {
         while (isGameActive)
@@ -49,7 +47,7 @@ public class GameManagerX : MonoBehaviour
         }
     }
 
-    // Generate a random spawn position based on a random index from 0 to 3
+    
     Vector3 RandomSpawnPosition()
     {
         float spawnPosX = minValueX + (RandomSquareIndex() * spaceBetweenSquares);
@@ -59,29 +57,25 @@ public class GameManagerX : MonoBehaviour
         return spawnPosition;
 
     }
-
-    // Generates random square index from 0 to 3, which determines which square the target will appear in
+    
     int RandomSquareIndex()
     {
         return Random.Range(0, 4);
     }
-
-    // Update score with value from target clicked
+    
     public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
         scoreText.text = "score " + score;
     }
-
-    // Stop game, bring up game over text and restart button
+    
     public void GameOver()
     {
         gameOverText.gameObject.SetActive(true);
         restartButton.gameObject.SetActive(true);
         isGameActive = false;
     }
-
-    // Restart game by reloading the scene
+    
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
