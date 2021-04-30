498338570d85a64ee8cc899a411a0ac69a28e4bd
Slight-san <10802552@my.uvu.edu>
2021-04-29 20:48:22 -0600

    code cleanup

--- Create With Code/Challenge 5/Assets/Challenge 5/Scripts/TargetX.cs
+++ Create With Code/Challenge 5/Assets/Challenge 5/Scripts/TargetX.cs
@@ -11,9 +11,9 @@ public class TargetX : MonoBehaviour
 
     public float timeOnScreen = 1.0f;
 
-    private float minValueX = -3.75f; // the x value of the center of the left-most square
-    private float minValueY = -3.75f; // the y value of the center of the bottom-most square
-    private float spaceBetweenSquares = 2.5f; // the distance between the centers of squares on the game board
+    private float minValueX = -3.75f; 
+    private float minValueY = -3.75f; 
+    private float spaceBetweenSquares = 2.5f; 
     
 
     void Start()
@@ -22,11 +22,10 @@ public class TargetX : MonoBehaviour
         gameManagerX = GameObject.Find("Game Manager").GetComponent<GameManagerX>();
 
         transform.position = RandomSpawnPosition(); 
-        StartCoroutine(RemoveObjectRoutine()); // begin timer before target leaves screen
+        StartCoroutine(RemoveObjectRoutine()); 
 
     }
-
-    // When target is clicked, destroy it, update score, and generate explosion
+    
     private void OnMouseDown()
     {
         if (gameManagerX.isGameActive)
@@ -37,8 +36,7 @@ public class TargetX : MonoBehaviour
         }
                
     }
-
-    // Generate a random spawn position based on a random index from 0 to 3
+    
     Vector3 RandomSpawnPosition()
     {
         float spawnPosX = minValueX + (RandomSquareIndex() * spaceBetweenSquares);
@@ -48,15 +46,13 @@ public class TargetX : MonoBehaviour
         return spawnPosition;
 
     }
-
-    // Generates random square index from 0 to 3, which determines which square the target will appear in
+    
     int RandomSquareIndex ()
     {
         return Random.Range(0, 4);
     }
 
-
-    // If target that is NOT the bad object collides with sensor, trigger game over
+    
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
@@ -67,14 +63,12 @@ public class TargetX : MonoBehaviour
         } 
 
     }
-
-    // Display explosion particle at object's position
+    
     void Explode ()
     {
         Instantiate(explosionFx, transform.position, explosionFx.transform.rotation);
     }
-
-    // After a delay, Moves the object behind background so it collides with the Sensor object
+    
     IEnumerator RemoveObjectRoutine ()
     {
         yield return new WaitForSeconds(timeOnScreen);
