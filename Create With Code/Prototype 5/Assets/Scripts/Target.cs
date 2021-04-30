97c7d0fcf2ca2128a6eb093f2722a6c57f7e6637
Slight-san <10802552@my.uvu.edu>
2021-04-29 18:24:50 -0600

    added OnMouseDown method and ontriggerenter method to destroy objects on mouse click and when they fall out of the scene (raised spawnPos to -2y)

--- Create With Code/Prototype 5/Assets/Scripts/Target.cs
+++ Create With Code/Prototype 5/Assets/Scripts/Target.cs
@@ -11,7 +11,7 @@ public class Target : MonoBehaviour
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
-    private float ySpawnPos = -6;
+    private float ySpawnPos = -2;
     
     void Start()
     {
@@ -26,6 +26,16 @@ public class Target : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 
     Vector3 RandomForce()
